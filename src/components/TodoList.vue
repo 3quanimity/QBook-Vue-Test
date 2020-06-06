171c98d22023d2a1f23c5d58e3ca5e2bc2def38b
Haythem Ben Drissia <BenDrissiaHaythem@gmail.com>
2020-06-06 07:59:24 +0100

    render mockup data

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>Todo list goes here</div>
+  <div>
+    <input
+      type="text"
+      class="todo-input"
+      placeholder="What needs to be done"
+      v-model="newTodo"
+    />
+    <div v-for="todo in todos" :key="todo.id" class="todo-item">
+      {{ todo.title }}
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "TodoList",
-  props: {
-    msg: String
-  }
+  data() {
+    return {
+      newTodo: "",
+      todos: [
+        {
+          id: 1,
+          title: "Finish test",
+          isComplete: false,
+          requiredTime: 60,
+        },
+        {
+          id: 2,
+          title: "have lunch",
+          isComplete: false,
+          requiredTime: 15,
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style>
+<style lang="scss">
+.todo-input {
+  width: 100%;
+  padding: 10px 18px;
+  font-size: 18px;
+  margin-bottom: 16px;
+
+  &:focus {
+    outline: 0;
+  }
+}
 </style>
